b8d00d570fa108ad78c8057c4dffcd430450b4e1
miguel-goncvs <miguel.rosa.goncalves@gmail.com>
2020-07-22 08:53:22 -0300

    hide menu toggler

--- src/components/SideWidget/index.js
+++ src/components/SideWidget/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import './style.css'
+import React, { useState } from 'react';
+import './style.css';
+import { FiChevronLeft as Icon} from 'react-icons/fi'
 
 const SideWidget = (props) => {
+    
+    const [open, setOpen] = useState(false)
+
     return(
-        <div id="SideWidget">
+        <div id="SideWidget" open={open} style={{transform: open === false ? 'translateX(0)' : 'translateX(-100%)'}}>
+            <Icon id="SideWidget-Toogle" open={open} onClick={() => { setOpen(!open); console.log(open )}}
+            style={{transform: open === false ? 'rotate(0)' : 'rotate(180deg)'}}/>    
+            
             <div className="SideWidget-Header">
             <h1>ondié</h1>
             <p> Encontre negócios próximos e<br/>incentive o comércio local</p>
             </div>
             <div id="SideWidget-Wrapper">
-                {
+            {
                     props.points.map( point => (
                         <div className="SideWidget-Point" key={point.id}>
                             <h2>{point.title.rendered}</h2>
@@ -34,6 +41,7 @@ const SideWidget = (props) => {
                         </div>
                     ))
                 }
+            
             </div>
         </div>
     )
