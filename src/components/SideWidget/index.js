6643ffd1d7bd8db234d38d7aebf7a3ebcfde24dd
miguel-goncvs <miguel.rosa.goncalves@gmail.com>
2020-07-21 21:12:20 -0300

    updating sidebar

--- src/components/SideWidget/index.js
+++ src/components/SideWidget/index.js
@@ -4,11 +4,20 @@ import './style.css'
 const SideWidget = (props) => {
     return(
         <div id="SideWidget">
+            <div className="SideWidget-Header">
+            <h1>ondié</h1>
+            <p> Encontre negócios próximos e<br/>incentive o comércio local</p>
+            </div>
             <div id="SideWidget-Wrapper">
                 {
                     props.points.map( point => (
                         <div className="SideWidget-Point" key={point.id}>
-                            {point.title.rendered}
+                            <h2>{point.title.rendered}</h2>
+                            <p>{point.acf.description}</p>
+                            <div>
+                                <a className="SideWidget-WhatsApp" target="_blank" target="_blank" rel="noopener noreferrer" href={`https://wa.me/${point.acf.whatsapp}`}>Whatsapp</a>
+                                <a className="SideWidget-Email" target="_blank" rel="noopener noreferrer" href={`mailto:${point.acf.email}`}>Email</a>
+                            </div>
                         </div>
                     ))
                 }
